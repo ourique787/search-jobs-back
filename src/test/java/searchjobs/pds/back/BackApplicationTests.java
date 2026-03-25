5e151df24a7cce5aba760d2d76dbc117b442cf88
ourique787 <lucastrajano93@gmail.com>
2026-03-25 14:57:47 -0300

    fix test scraping

--- src/test/java/searchjobs/pds/back/BackApplicationTests.java
+++ src/test/java/searchjobs/pds/back/BackApplicationTests.java
@@ -3,10 +3,15 @@ package searchjobs.pds.back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import searchjobs.pds.back.services.ScraperService;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BackApplicationTests {
+
+	@MockitoBean
+	private ScraperService scraperService;
 
 	@Test
 	void contextLoads() {

a89a4aa7defaf39bd26b0a5759a82963f95b8391
ourique787 <lucastrajano93@gmail.com>
2026-05-24 18:08:03 -0300

    fix scraper bugs

--- src/test/java/searchjobs/pds/back/BackApplicationTests.java
+++ src/test/java/searchjobs/pds/back/BackApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import searchjobs.pds.back.config.DatabaseSeeder;
+import searchjobs.pds.back.services.EmpregosBrScraperService;
+import searchjobs.pds.back.services.InfoJobsScraperService;
+import searchjobs.pds.back.services.JobEnricherService;
 import searchjobs.pds.back.services.ScraperService;
 
 @SpringBootTest
@@ -11,7 +15,19 @@ import searchjobs.pds.back.services.ScraperService;
 class BackApplicationTests {
 
 	@MockitoBean
+	private DatabaseSeeder databaseSeeder;
+
+	@MockitoBean
 	private ScraperService scraperService;
+
+	@MockitoBean
+	private InfoJobsScraperService infoJobsScraperService;
+
+	@MockitoBean
+	private EmpregosBrScraperService empregosBrScraperService;
+
+	@MockitoBean
+	private JobEnricherService jobEnricherService;
 
 	@Test
 	void contextLoads() {
